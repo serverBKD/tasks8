a37b45ea70dbd0c84239e06d7a891ccd14f9940a
serverBKD <carlosm1bravoa@gmail.com>
2025-07-05 01:07:36 -0400

    2025JUL05: v0.1.9: CSS: Cards + LinkBar + Icon.js + box-shadow

--- src/componentes/card-tasks.jsx
+++ src/componentes/card-tasks.jsx
@@ -1,59 +1,42 @@
 import Image from "next/image";
-import LinkBar from "../componentes/link-bar.jsx"
+import LinkBar from "../componentes/link-bar.jsx";
 // import { IconsBarcode } from "../../public/icons/icons-hugeicons";
 
 const ImagePlaceholder = "/assets/placeholder-image.webp"; // Placeholder image path
 
 export default function CardTasks({ tasks }) {
-	console.log("fromCardTasks", tasks);
-	return (
-		<section className="card_container">
-			{tasks?.map((task) => (
-				<article className="card_box" key={task?.id-Math.random()}>
-					{/* Imagen */}
-					<div className="w-full">
-						{!task?.img ?
-						(<Image
-								src={ImagePlaceholder}
-								alt={task?.concept}
-								width={1080}
-								height={1080}
-								className="card_image"	
-						/>)
-						: (
-							<Image
-								src={task?.img}
-								alt={task?.concept}
-								width={1080}
-								height={1080}
-								className="card_image"
-							/>
-						)}
-					</div>
-					{/* <!-- Card like YT --> */}
-					<div className="card_info">
-						<h4
-							className={`${
-								task.amount < 0 ? "text-x-error" : "text-x-color"
-							} tracking-wider text-3xl`}
-						>
-							{task.amount === 0 ? 0 : task.amount}
-						</h4>
-						<p
-							style={{
-								textDecoration: task?.completed ? "line-through" : "none",
-							}}
-						>
-							{task.concept}
-						</p>
-						<h5>{task?.initAt}</h5>
-					</div>
-					{/* <!-- Card like YT --> */}
-					<div>
-						<LinkBar/>
-					</div>
-				</article>
-			))}
-		</section>
-	);
+  console.log("fromCardTasks", tasks);
+  return (
+    <section className="card_container">
+      {tasks?.map((task) => (
+        <article className="card_box" key={task?.id - Math.random()}>
+          {/* Imagen */}
+          <div className="w-full">
+            {!task?.img ? (
+              <Image src={ImagePlaceholder} alt={task?.concept} width={1080} height={1080} className="card_image" />
+            ) : (
+              <Image src={task?.img} alt={task?.concept} width={1080} height={1080} className="card_image" />
+            )}
+          </div>
+          {/* <!-- Card like YT --> */}
+          <div className="card_info">
+            <p
+              style={{
+                textDecoration: task?.completed ? "line-through" : "none",
+              }}
+            >
+              {task.concept}
+            </p>
+            <h4 className={`${task.amount < 0 ? "text-x-error" : "text-x-color"} tracking-wider text-3xl`}>
+              {task.amount === 0 ? 0 : task.amount}
+            </h4>
+
+            <h5>{task?.initAt}</h5>
+          </div>
+          {/* <!-- Card like YT --> */}
+          <LinkBar />
+        </article>
+      ))}
+    </section>
+  );
 }
--- src/componentes/link-bar.jsx
+++ src/componentes/link-bar.jsx
@@ -1,8 +1,25 @@
+"use client";
+import { GlobeIcon, YouTubeIcon, GitHubIcon } from "../services/iconos.js";
+import Link from "next/link.js";
 
 export default function LinkBar() {
   return (
-    <section>
-        <h3>LinkBar</h3>
+    <section className="w-full flex flex-wrap gap-2 justify-evenly items-center ">
+      <Link
+        href="/"
+        className="w-28 h-12 flex items-center justify-start px-2 py-4 outline-2 outline-x-link text-x-link rounded-lg shadow-md hover:cursor-pointer hover:bg-x-link/10 transition-all duration-300"
+      >
+        <GlobeIcon />
+        <h3 className="text-xs">SOURCE</h3>
+      </Link>
+      <div className="w-28 h-12 flex items-center justify-start px-2 py-4 outline-2 outline-x-link text-x-link rounded-lg shadow-md hover:cursor-pointer hover:bg-x-link/10 transition-all duration-300">
+        <GitHubIcon />
+        <h3 className="text-xs">REPO</h3>
+      </div>
+      <div className="w-28 h-12 flex items-center justify-start px-2 py-4 outline-2 outline-x-link text-x-link rounded-lg shadow-md hover:cursor-pointer hover:bg-x-link/10 transition-all duration-300">
+        <YouTubeIcon />
+        <h3 className="text-xs">LinkBar</h3>
+      </div>
     </section>
-  )
+  );
 }
